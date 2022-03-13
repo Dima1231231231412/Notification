b4887d97ac7a8b23ff82deb9376c2750b7a6bc81
Dima <93972220+Dima1231231231412@users.noreply.github.com>
2022-03-13 15:39:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import win10toast
+
+toast = win10toast.ToastNotifier() ##Создание объекта класса
+toast.show_toast(title='Проснись',msg='Пора учиться',duration=10) ##Заголовок, сообщение, время
+
+
